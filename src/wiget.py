e471fd613fc2b13615f3086978a32d45c13509ef
Ботт Владимир <Vladimirbott@mail.ru>
2025-07-01 12:49:59 -0700

    Обновление кода для скрытия счета

--- src/wiget.py
+++ src/wiget.py
@@ -31,16 +31,6 @@ def mask_account_card(info: str) -> str:
             return info  # Если номер карты некорректен, возвращаем исходную строку
 
 
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-
-
 def get_date(date_str: str) -> str:
     """Преобразует дату из формата '2024-03-11T02:26:18.671407' в '11.03.2024'"""
     # Беру только часть до буквы T (это будет дата)
@@ -51,7 +41,3 @@ def get_date(date_str: str) -> str:
 
     # Собираем в новом формате
     return f"{day}.{month}.{year}"
-
-
-print(get_date("2024-03-11T02:26:18.671407"))
-print(get_date("1999-12-31T23:59:59.999999"))
