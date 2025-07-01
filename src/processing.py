ebe616bc48d35b3c05f610158167fa324ebd47ec
Ботт Владимир <Vladimirbott@mail.ru>
2025-07-01 12:43:08 -0700

    Сортировка по data and state, обновленная

--- src/processing.py
+++ src/processing.py
@@ -13,27 +13,6 @@ def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dic
     return result
 
 
-operations = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-print(filter_by_state(operations))  # Вызов с параметром по умолчанию (EXECUTED)
-print(filter_by_state(operations, state="CANCELED"))  # Вызов с указанием статуса CANCELED
-
-
 def sort_by_date(transactions: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Сортирует список словарей по ключу 'date'"""
     return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
-
-
-transactions: List[Dict[str, Any]] = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-]
-
-sorted_transactions: List[Dict[str, Any]] = sort_by_date(transactions)
-print(sorted_transactions)
